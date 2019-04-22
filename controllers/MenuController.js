c1e49c92808f7edd8b0ce4ee466e740c689379d2
Sarah Rothwell <sarahhrothwell@gmail.com>
2019-04-22 15:28:03 -0400

    first commit

---
+++ controllers/MenuController.js
@@ -0,0 +1,80 @@
+const inquirer = require('inquirer');
+const ContactController = require("./ContactController");
+const moment = require('moment');
+
+module.exports = class MenuController {
+  constructor(){
+    this.mainMenuQuestions = [
+      {
+        type: "list",
+        name: "mainMenuChoice",
+        message: "Please choose from an option below: ",
+        choices: [
+          "Add new contact",
+          "Get date",
+          "Exit"
+        ]
+      }
+    ];
+    this.book = new ContactController();
+  }
+
+  main(){
+    console.log(`Welcome to AddressBloc!`);
+    inquirer.prompt(this.mainMenuQuestions).then((response) => {
+      switch(response.mainMenuChoice){
+        case "Add new contact":
+          this.addContact();
+          break;
+        case "Get date":
+          this.getDate();
+          break;
+        case "Exit":
+          this.exit();
+        default:
+          console.log("Invalid input");
+          this.main();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  clear(){
+    console.log("\x1Bc");
+  }
+
+  addContact(){
+    this.clear();
+    inquirer.prompt(this.book.addContactQuestions).then((answers) => {
+      this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
+        console.log("Contact added successfully!");
+        this.main();
+      }).catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    });
+    }
+
+  getDate(){
+    this.clear();
+    let a = moment().format('MMMM Do YYYY, h:mm:ss a');
+    console.log(a);
+    this.main();
+  }
+
+  exit(){
+    console.log("Thanks for using AddressBloc!");
+    process.exit();
+  }
+
+  getContactCount(){
+    return this.contacts.length;
+  }
+
+  remindMe(){
+    return "Learning is a life-long pursuit";
+  }
+}
